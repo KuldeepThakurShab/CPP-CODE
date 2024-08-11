be9e582bc7e996b9ea32bb71b8610779124b813f
KULDEEP TOMAR <98374974+KuldeepThakurShab@users.noreply.github.com>
2024-08-11 10:43:37 +0000

    01_Basics

---
+++ 01_Basics/hello.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+    cout<<"This is the first c++ Program";
+    return 0;
+}
--- first.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "hello this is the fisrt cpp program" << endl;
-
-    return 0;
-}
